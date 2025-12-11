95d02f45ceee77a49737d37e6165d5504b06f5a5
P0kay <jszymanski373@gmail.com>
2025-12-11 22:34:15 +0100

    database connection and use template

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,7 +1,21 @@
+import { neon } from '@neondatabase/serverless';
+import { v4 as uuidv4 } from 'uuid';
+
 function SignUp() {
-    return ( 
-        <>signup</>
-     );
+    async function create(formData) {
+        'use server';
+        // Connect to the Neon database
+        const sql = neon(`${process.env.DATABASE_URL}`);
+        const comment = formData.get('comment');
+        // Insert the comment from the form into the Postgres database
+        await sql.query('INSERT INTO "user" VALUES ($1,$2,$3,$4)', [uuidv4(),"email",'password','type']);
+    }
+    return (
+        <form action={create}>
+            <input type="text" placeholder="write a comment" name="comment" />
+            <button type="submit">Submit</button>
+        </form>
+    );
 }
 
-export default SignUp;
+export default SignUp;``
